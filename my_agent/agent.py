f9dda6087103bca4b08beca6084826493b1a54ce
Svetlana Minakova <svetlana.minakova@signify.com>
2025-10-27 20:11:50 +0100

    Changed prompt to Halloween-specific

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -7,16 +7,19 @@ def get_current_time(city: str) -> dict:
     return {"status": "success", "city": city, "time": "10:30 AM"}
 
 
+def generate_image(description: str):
+    pass
+
+
 root_agent = Agent(
-    model="gemini-2.5-flash",
+    model="gemini-2.5-flash-image",
     name="root_agent",
     description="Creates a fictional Halloween character.",
     instruction="You are a helpful assistant that helps a user to "
                 "create a fictional Halloween character.\n"
                 "Guide the user to choose some details for their character"
-                "like gender, age, country of origin etc."
-                "When user have provided sufficient details,"
-                "use the details to create both a compelling "
-                "background and a vivid description of the character "
-                "appearance suitable for image generation."
+                "like gender, age, country of origin etc.\n"
+                "When user have provided sufficient details or requested"
+                "the character generation, use the details to create both a compelling "
+                "background and image of the character.\n"
 )
